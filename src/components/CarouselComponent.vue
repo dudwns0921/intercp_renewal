287dca7185d7361be174bf3a578d06fa6ae84a54
Elio Jung <dudwns0921@gmail.com>
2022-10-04 11:32:57 +0900

    feat: carousel 컴포넌트 최종 완성

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -1,27 +1,62 @@
 <script lang="ts" setup>
-import { ref, defineProps } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const props = defineProps({
   imgFiles: Array,
 });
+const isActiveObj = ref({ currentIdx: 0 });
 
-let imgIdx = ref(0);
+onMounted(() => {
+  for (let i = 0; i < props.imgFiles?.length; i++) {
+    isActiveObj.value[`isActive${i}`] = false;
+  }
+  isActiveObj.value.isActive0 = true;
+});
 
-function changeIndex(index: number): void {
-  imgIdx.value = index;
+function returnIsActive(index) {
+  return isActiveObj.value[`isActive${index}`];
+}
+
+function changeImgIdx(index: number): void {
+  isActiveObj.value[`isActive${isActiveObj.value.currentIdx}`] = false;
+  isActiveObj.value[`isActive${index}`] = true;
+  isActiveObj.value.currentIdx = index;
 }
 </script>
 
 <template>
   <div class="carousel__img-container">
-    <img :src="props.imgFiles[imgIdx.valueOf()]" />
-  </div>
-  <div class="carousel__control">
-    <div
-      :key="item"
+    <img
       v-for="(item, index) in props.imgFiles"
-      @click="changeIndex(index)"
-    ></div>
+      :key="item"
+      :src="item"
+      :class="[
+        'carousel__img--default',
+        {
+          'carousel__img--active': returnIsActive(index),
+        },
+      ]"
+    />
+    <div class="carousel__txt-container">
+      열방을 향한 끊이지 않는 기도,
+      <br />
+      열방을 위한 끊이지 않는 찬양,
+      <br />
+      <div class="carousel__txt--highlighted">인터CP</div>
+    </div>
+    <div class="carousel__control">
+      <div
+        v-for="(item, index) in props.imgFiles"
+        :key="index"
+        :class="[
+          'carousel__control-dot--default',
+          {
+            'carousel__control-dot--active': returnIsActive(index),
+          },
+        ]"
+        @click="changeImgIdx(index)"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -31,26 +66,53 @@ function changeIndex(index: number): void {
   width: 100%;
   height: 800px;
 
-  img {
+  .carousel__txt-container {
+    position: absolute;
+    color: white;
+    font-size: 36px;
+    top: 292px;
+    left: 438px;
+    text-shadow: 6px 3px 6px #17171766;
+
+    .carousel__txt--highlighted {
+      margin-top: 65px;
+      font-size: 48px;
+      font-weight: 400;
+    }
+  }
+
+  .carousel__img--default {
+    position: absolute;
     width: 100%;
     height: 100%;
     object-fit: cover;
+    opacity: 0;
+    transition: opacity 500ms ease-in-out;
+  }
+  .carousel__img--active {
+    opacity: 1;
   }
 }
 
 .carousel__control {
   position: absolute;
   display: grid;
-  grid-row-gap: 26px;
+  grid-row-gap: 20px;
   top: 50%;
   right: 380px;
-  height: 100px;
-  div {
-    background-color: black;
+
+  .carousel__control-dot--default {
+    background-color: #dfdfdf;
     cursor: pointer;
     width: 12px;
     height: 12px;
     border-radius: 50%;
+  }
+
+  .carousel__control-dot--active {
+    height: 52px;
+    background-color: #3e74ba;
+    border-radius: 30px;
   }
 }
 </style>
--- src/mock/imgFiles.ts
+++ src/mock/imgFiles.ts
@@ -2,6 +2,7 @@ const imgFiles: String[] = [
   'https://source.unsplash.com/78A265wPiO4',
   'https://source.unsplash.com/odxB5oIG_iA',
   'https://source.unsplash.com/y2azHvupCVo',
+  'https://source.unsplash.com/-kZo8vfI2wE',
 ];
 
 export default imgFiles;
