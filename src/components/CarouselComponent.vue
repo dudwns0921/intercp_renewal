f245247468ab73313ce0462168344713aff42e57
Elio Jung <dudwns0921@gmail.com>
2022-11-18 17:07:37 +0900

    fix: carousel 컴포넌트 수정
    swiper 라이브러리 사용

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -1,104 +1,86 @@
 <template>
-  <div class="carousel__img-container">
-    <img
-      v-for="(item, index) in props.imgFiles"
-      :key="item"
-      :src="item"
-      :class="[
-        'carousel__img--default',
-        {
-          'carousel__img--active': index === currentIdx,
-        },
-      ]"
-    />
-    <div class="carousel__txt-container">
-      <p>열방을 향한 끊이지 않는 기도,</p>
-      <p>열방을 위한 끊이지 않는 찬양,</p>
-      <p class="carousel__txt--highlighted">인터CP</p>
-    </div>
-    <div class="carousel__control">
-      <div
-        v-for="(item, index) in props.imgFiles"
-        :key="item"
-        :class="[
-          'carousel__control-dot--default',
-          {
-            'carousel__control-dot--active': index === currentIdx,
-          },
-        ]"
-        @click="currentIdx = index"
-      ></div>
-    </div>
+  <swiper
+    :modules="modules"
+    :slides-per-view="1"
+    :space-between="50"
+    navigation
+    :pagination="{ clickable: true }"
+  >
+    <SwiperSlide v-for="item in imgFiles" :key="item">
+      <div class="carousel__img-container">
+        <img :src="item" /></div
+    ></SwiperSlide>
+  </swiper>
+  <div class="carousel__txt-container">
+    <p>열방을 향한 끊이지 않는 기도,</p>
+    <p>열방을 위한 끊이지 않는 찬양,</p>
+    <p class="carousel__txt--highlighted">인터CP</p>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
-const props = defineProps({
-  imgFiles: Array,
-});
-const currentIdx = ref(0);
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      currentIdx: 0,
+    };
+  },
+  props: {
+    imgFiles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  setup() {
+    return {
+      modules: [Navigation, Pagination, Scrollbar, A11y],
+    };
+  },
+};
 </script>
 
 <style scoped lang="scss">
 .carousel__img-container {
-  position: relative;
-  width: 100%;
   height: 650px;
-
-  .carousel__txt-container {
-    position: absolute;
-    color: white;
-    font-size: 36px;
-    top: 292px;
-    left: 438px;
-    text-shadow: 6px 3px 6px #17171766;
-
-    .carousel__txt--highlighted {
-      margin-top: 65px;
-      font-size: 48px;
-    }
-  }
-
-  .carousel__img--default {
-    position: absolute;
+  // carousel 높이 조정하기
+  img {
     width: 100%;
     height: 100%;
-    object-fit: cover;
-    opacity: 0;
-    transition: opacity 500ms ease-in-out;
-  }
-
-  .carousel__img--active {
-    opacity: 1;
   }
 }
-
-.carousel__control {
+.carousel__txt-container {
   position: absolute;
-  display: grid;
-  grid-row-gap: 20px;
-  top: 50%;
-  right: 380px;
+  color: white;
+  font-size: 36px;
+  top: 292px;
+  left: 438px;
+  text-shadow: 6px 3px 6px #17171766;
+  z-index: 2;
 
-  .carousel__control-dot--default {
-    background-color: #dfdfdf;
-    cursor: pointer;
-    width: 12px;
-    height: 12px;
-    border-radius: 50%;
-  }
-
-  .carousel__control-dot--active {
-    height: 52px;
-    background-color: #3e74ba;
-    border-radius: 30px;
+  .carousel__txt--highlighted {
+    margin-top: 65px;
+    font-size: 48px;
   }
 }
 
 @media (max-width: 800px) {
-  .carousel__img-container .carousel__txt-container {
+  .carousel__txt-container {
     left: 1%;
     top: 1%;
   }
